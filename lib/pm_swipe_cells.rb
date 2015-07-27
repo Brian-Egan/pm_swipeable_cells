fc497bad9a6ba6ab3821af8461918f30e09d764d
Brian Egan (MBPro) <bfegan@>
2015-07-27 16:06:44 -0400

    Playing with options

--- lib/pm_swipe_cells.rb
+++ lib/pm_swipe_cells.rb
@@ -3,14 +3,20 @@ unless defined?(Motion::Project::Config)
 end
 
 require 'motion-cocoapods'
+require ''
 
 Motion::Project::App.setup do |app|
+
+app.name = "pm_swipe_cells"
 
   app.pods do
     pod 'SWTableViewCell'
   end
 
-  Dir.glob(File.join(File.dirname(__FILE__), 'pm_swipe_cell/*.rb')).each do |file|
+  # lib_dir_path = File.dirname(File.expand_path(__FILE__))
+  # app.files.unshift(Dir.glob(File.join(lib_dir_path, "timestamps/**/*.rb")))
+
+  Dir.glob(File.join(File.dirname(__FILE__), 'pm_swipe_cells/*.rb')).each do |file|
     app.files.unshift(file)
   end
 
